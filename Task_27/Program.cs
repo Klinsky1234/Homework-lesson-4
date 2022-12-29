8c440d9675f0807003d6d114b81a25bb43b41ed9
Dmitriy Kalininskiy <kalininskiyds@gmail.com>
2022-12-29 19:39:43 +0300

    Task27

---
+++ Task_27/Program.cs
@@ -0,0 +1,20 @@
+﻿/*  27. Напишите прогу, которая принимает на вход число 
+и выдаёт сумму цифр в числе
+452 -> 11
+82 -> 10
+9012 -> 12
+*/
+
+int a;
+Console.Write($" Ведите число: ");
+int.TryParse(Console.ReadLine(), out a);
+int b = a;
+int sum = 0;
+
+while (b > 0)
+{
+    sum = sum + b % 10;
+    b /= 10;
+}
+
+Console.WriteLine($" {a} -> {sum}");
