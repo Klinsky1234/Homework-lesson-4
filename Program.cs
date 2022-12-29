773bcb72525c9cdbf9b8f845dab96f6a1aecbbed
Dmitriy Kalininskiy <kalininskiyds@gmail.com>
2022-12-29 19:55:14 +0300

    Task 29

---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿/* 29. Напишите программу, которая задаёт массив из 8 элекментов и выводит их на экран */
+
+int[] array = new int[8];
+for (int i = 0; i < 8; i++)
+{
+    array[i] = new Random().Next(100);
+    Console.Write($" [{i}] = {array[i]}\n ");
+}
